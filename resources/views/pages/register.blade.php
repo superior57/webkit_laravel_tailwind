adbec257409f842deb5a0d96c51711cc73d26ae1
Elite <petpanda0057@gmail.com>
2023-06-06 16:46:47 +0300

    feat: add Register step 1 page.

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -3,9 +3,38 @@
 @section('title', 'Register')
 
 @section('content')
-  <div class="container mx-auto p-4">
-    <h1 class="text-primary">Register page</h1>
+  @php
+    $options = [
+      'Designer UX',
+      'Designer UI',
+      'Développeur front',
+      'Développeur back',
+      'Étudiant'
+    ];    
+  @endphp
+  <div class="container max-w-sm mx-auto">
+    <h3 class="mb-40 title-3 text-main-primary">Inscription</h3>
 
-    <p class="mb-4 leading-relaxed">This is Register page</p>
+    <p class="mb-24 subtitle-2">1/2 - Vous êtes…</p>
+
+    <div class="mb-40">
+      @foreach ($options as $option)
+      <div class="radio">
+        <input type="radio" name="type" id="radio-{{ $option }}">
+        <label for="radio-{{ $option }}">
+          <div class="option-outline">
+            <span class="active"></span>
+          </div>
+          <span class="option-label border-b-p-default border-b-1">
+            {{ $option }}
+          </span>
+        </label>
+      </div>          
+      @endforeach
+    </div>
+
+    <button class="btn btn-primary btn-medium">Question suivante
+      @svg('assets/svg/icon-next.svg', 'ml-12')
+    </button>
   </div>
 @endsection
