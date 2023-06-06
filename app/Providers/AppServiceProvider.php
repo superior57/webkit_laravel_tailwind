9b0bf497cc6507a7d80c2660b3288037c2aa8642
Elite <petpanda0057@gmail.com>
2023-06-06 16:43:20 +0300

    feat: add SVG directive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,19 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        \Blade::directive('svg', function($arguments) {
+            // Funky madness to accept multiple arguments into the directive
+            list($path, $class) = array_pad(explode(',', trim($arguments, "() ")), 2, '');
+            $path = trim($path, "' ");
+            $class = trim($class, "' ");
+    
+            // Create the dom document as per the other answers
+            $svg = new \DOMDocument();
+            $svg->load(public_path($path));
+            $svg->documentElement->setAttribute("class", $class);
+            $output = $svg->saveXML($svg->documentElement);
+    
+            return $output;
+        });
     }
 }
