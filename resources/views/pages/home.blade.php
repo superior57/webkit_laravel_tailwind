f410a5e8bc9c5722b68d824783fe983a0880d6d3
Elite <petpanda0057@gmail.com>
2023-06-06 16:56:09 +0300

    feat: add register button on Home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,5 +7,7 @@
     <h1 class="text-2xl font-bold mb-4">Home page</h1>
 
     <p class="mb-4 leading-relaxed">This is Home page</p>
+
+    <a href="{{ url('/register') }}" class="btn btn-primary btn-small m-30 max-w-sm ml-auto">Go to Register</a>
   </div>
 @endsection
